259003666a3e3435f82cecbb9d09290fdf6fe637
kingkazuma7 <nasionasio7@gmail.com>
2018-11-25 17:11:58 +0900

    181125 Sec10 グロナビの作成
    p109~103

--- design-wp/design-wp/wp-content/themes/hotel/header.php
+++ design-wp/design-wp/wp-content/themes/hotel/header.php
@@ -35,10 +35,11 @@ wp_head();
     <?php endif; ?>
 
     <nav class="globalNavi">
-        <ul>
-            <li><a href="index.html">HOME</a></li>
-            <li><a href="about.html">ホテル紹介</a></li>
-            <li><a href="access.html">アクセス</a></li>
-            <li><a href="contact.html">お問い合わせ</a></li>
-        </ul>
+    <?php
+    $args = array(
+        'menu' => 'global-navigation',
+        'container' => false,
+    );
+    wp_nav_menu($args);
+    ?>
     </nav><!-- /.globalNavi -->
