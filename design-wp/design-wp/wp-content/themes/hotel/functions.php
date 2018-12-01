dcae70cbc40f0e7abcc384cb434abe8f80f6e185
kingkazuma7 <nasionasio7@gmail.com>
2018-12-01 22:47:46 +0900

    [Change] コメント欄
    続きは「p161のコメントが投稿されているか調べる」〜

--- design-wp/design-wp/wp-content/themes/hotel/functions.php
+++ design-wp/design-wp/wp-content/themes/hotel/functions.php
@@ -8,3 +8,14 @@ add_theme_support( 'post-thumbnails' );
  * カスタムメニュー機能を使用可能にする
  */
 add_theme_support( 'menus' );
+
+/**
+ * コメント投稿フォームの「名前」「メールアドレス」「ウェブサイト」の項目を削除する
+ */
+add_filter('comment_form_default_fields', 'my_comment_form_default_fields');
+function my_comment_form_default_fields( $args ) {
+  $args['author'] = '';
+  $args['email'] = '';
+  $args['url'] = '';
+  return $args;
+}
